e1725fabdb9c121fb5f40f33e8c4e1a7e131cde0
Varun Nath <nathvarun@hotmail.com>
2019-06-05 18:28:18 +0530

    Create a searchable contact list. :phone:

--- App.js
+++ App.js
@@ -1,28 +1,124 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
-import LoginScreen from './screens/LoginScreen';
-import DashboardScreen from './screens/DashboardScreen';
-import LoadingScreen from './screens/LoadingScreen';
-
-import * as firebase from 'firebase';
-import { firebaseConfig } from './config';
-firebase.initializeApp(firebaseConfig);
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator
+} from 'react-native';
+import { Contacts } from 'expo';
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      contacts: []
+    };
+  }
+
+  loadContacts = async () => {
+    const permission = await Expo.Permissions.askAsync(
+      Expo.Permissions.CONTACTS
+    );
+
+    if (permission.status !== 'granted') {
+      return;
+    }
+
+    const { data } = await Contacts.getContactsAsync({
+      fields: [Contacts.Fields.PhoneNumbers, Contacts.Fields.Emails]
+    });
+
+    console.log(data);
+    this.setState({ contacts: data, inMemoryContacts: data, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    this.loadContacts();
+  }
+
+  renderItem = ({ item }) => (
+    <View style={{ minHeight: 70, padding: 5 }}>
+      <Text style={{ color: '#bada55', fontWeight: 'bold', fontSize: 26 }}>
+        {item.firstName + ' '}
+        {item.lastName}
+      </Text>
+      <Text style={{ color: 'white', fontWeight: 'bold' }}>
+        {item.phoneNumbers[0].digits}
+      </Text>
+    </View>
+  );
+
+  searchContacts = value => {
+    const filteredContacts = this.state.inMemoryContacts.filter(contact => {
+      let contactLowercase = (
+        contact.firstName +
+        ' ' +
+        contact.lastName
+      ).toLowerCase();
+
+      let searchTermLowercase = value.toLowerCase();
+
+      return contactLowercase.indexOf(searchTermLowercase) > -1;
+    });
+    this.setState({ contacts: filteredContacts });
+  };
+
   render() {
-    return <AppNavigator />;
+    return (
+      <View style={{ flex: 1 }}>
+        <SafeAreaView style={{ backgroundColor: '#2f363c' }} />
+        <TextInput
+          placeholder="Search"
+          placeholderTextColor="#dddddd"
+          style={{
+            backgroundColor: '#2f363c',
+            height: 50,
+            fontSize: 36,
+            padding: 10,
+            color: 'white',
+            borderBottomWidth: 0.5,
+            borderBottomColor: '#7d90a0'
+          }}
+          onChangeText={value => this.searchContacts(value)}
+        />
+        <View style={{ flex: 1, backgroundColor: '#2f363c' }}>
+          {this.state.isLoading ? (
+            <View
+              style={{
+                ...StyleSheet.absoluteFill,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <ActivityIndicator size="large" color="#bad555" />
+            </View>
+          ) : null}
+          <FlatList
+            data={this.state.contacts}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            ListEmptyComponent={() => (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  marginTop: 50
+                }}
+              >
+                <Text style={{ color: '#bad555' }}>No Contacts Found</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    );
   }
 }
-
-const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoadingScreen,
-  LoginScreen: LoginScreen,
-  DashboardScreen: DashboardScreen
-});
-
-const AppNavigator = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
